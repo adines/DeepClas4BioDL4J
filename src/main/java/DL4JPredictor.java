7117c6ffed0dc937e7d119c1c575bafffcf713ad
adines <adrian140793@hotmail.com>
2018-07-13 13:14:49 +0200

    PredictBatch added

--- src/main/java/DL4JPredictor.java
+++ src/main/java/DL4JPredictor.java
@@ -1,6 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
 import org.deeplearning4j.nn.graph.ComputationGraph;
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
 import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.factory.Nd4j;
 
 /**
  *
@@ -33,6 +37,49 @@ public class DL4JPredictor extends Predictor{
             MultiLayerNetwork graph=(MultiLayerNetwork)m;
             INDArray output=graph.output(input);
             return model.getPostProcessor().apply(output);
+        }
+    }
+
+    @Override
+    public INDArray[] predictBatch(String[] images) {
+        Model model=this.getModel();
+        INDArray input;
+        List <INDArray> linputs=new ArrayList<INDArray>();
+        Function<String,INDArray> f= model.getPreProcessor();
+        for(String image:images)
+        {
+            input=model.getPreProcessor().apply(image);
+            linputs.add(input);
+        }
+        INDArray []inputs=new INDArray[linputs.size()];
+        inputs=linputs.toArray(inputs);
+        
+        org.deeplearning4j.nn.api.Model m=model.getDeepModel();
+        
+        if(m instanceof ComputationGraph)
+        {
+            ComputationGraph graph=(ComputationGraph)m;
+            INDArray[] output=graph.output(false,inputs);
+            for(int i=0;i<output.length;i++)
+            {
+                output[i]=Nd4j.sortWithIndices(output[i], 0, false)[0];
+            }
+            return output;
+        }
+            
+        else
+        {
+            MultiLayerNetwork graph=(MultiLayerNetwork)m;
+            List <INDArray> loutputs=new ArrayList<INDArray>();
+            for (INDArray i:inputs)
+            {
+                INDArray output=graph.output(i);
+                output=Nd4j.sortWithIndices(output,0,false)[0];
+                loutputs.add(output);
+            }
+            INDArray []outputs=new INDArray[loutputs.size()];
+            outputs=loutputs.toArray(outputs);
+            return outputs;
         }
     }
     
--- src/main/java/IPredictor.java
+++ src/main/java/IPredictor.java
@@ -1,9 +1,14 @@
 
+import org.nd4j.linalg.api.ndarray.INDArray;
+
+
 /**
  *
  * @author adines
  */
 public interface IPredictor {
     public abstract String predict(String image);
+    
+    public abstract INDArray[] predictBatch(String []images);
     
 }
